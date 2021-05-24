8b20eba040af9a6df2b408a81ed6bd5cd1d1eb27
Diego <diego.poveda@ceiba.com.co>
2021-05-23 20:16:18 -0500

    Agregados puntos y comas faltantes

--- microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
+++ microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
@@ -1,3 +1,3 @@
-insert into usuario(nombre,clave,fecha_creacion) values('test','1234',now())
+insert into usuario(nombre,clave,fecha_creacion) values('test','1234',now());
 
-insert into pelicula(nombre, autor, descripcion, esta_reservada) values ('Prueba', 'Autor', 'Descripcion', 'F')
+insert into pelicula(nombre, autor, descripcion, esta_reservada) values ('Prueba', 'Autor', 'Descripcion', 'F');
